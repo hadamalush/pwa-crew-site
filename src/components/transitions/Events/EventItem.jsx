f2489be397e19883ea09f2514081db80fff647c9
poncy <poncyman@gmail.com>
2023-09-29 11:11:11 +0200

    Added ornamentations for eventItem ,added styles to eventItem for desktop

--- src/components/transitions/Events/EventItem.jsx
+++ src/components/transitions/Events/EventItem.jsx
@@ -1,8 +1,8 @@
 "use client";
 import IconRender from "@/components/Icons/IconRender";
-import styles from "../../../styles/components/transitions/Events/EventItem.module.scss";
 import ImageFill from "../Image/ImageFill";
 import LinkAsBtn from "../Link/LinkAsBtn";
+import styles from "../../../styles/components/transitions/Events/EventItem.module.scss";
 
 const EventItem = ({
 	className,
@@ -21,7 +21,7 @@ const EventItem = ({
 	const showDetailHandler = () => {
 		const detailsList = document.getElementById(id);
 
-		detailsList.classList.toggle(styles["accordion__control"]);
+		detailsList.classList.toggle(styles["event-item__accordion--control"]);
 	};
 
 	return (
@@ -32,22 +32,22 @@ const EventItem = ({
 					alt='test'
 					className={styles["event-item__img"]}
 				/>
-
 				<div className={styles["event-item__content"]}>
 					<p className={styles["event-item__town"]}>{town}</p>
 					<address className={styles["event-item__date"]}>{date}</address>
 					<p>Start wydarzenia:</p>
-
 					<time className={styles["event-item__time"]}>{time}</time>
 				</div>
 			</div>
-			<div className={styles.accordion} id={id}>
+
+			<div className={styles["event-item__accordion"]} id={id}>
 				<ul className={styles["details"]}>
 					<li className={styles["details__item"]}>
 						<IconRender
 							variant='location'
 							className={styles["details__item-icon"]}
 						/>
+
 						<address>
 							<p>{town}</p>
 							<p>{codePost}</p>
