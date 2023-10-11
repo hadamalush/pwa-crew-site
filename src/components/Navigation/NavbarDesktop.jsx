534acbf27cf4a2f300948a93b49f7307ab3e1593
poncy <poncyman@gmail.com>
2023-10-11 13:59:25 +0200

    Removed router push

--- src/components/Navigation/NavbarDesktop.jsx
+++ src/components/Navigation/NavbarDesktop.jsx
@@ -1,27 +1,16 @@
-"use client";
-
 import styles from "../../styles/components/Navigation/NavbarDesktop.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import { signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 const NavbarDesktop = ({ disc, lang, ...props }) => {
-	const { trl_home, trl_events, trl_contact, trl_login, trl_chat } = disc;
+	const { trl_home, trl_events, trl_contact, trl_login, trl_chat } = disc; //Translation
 
 	const classes = `${styles.nav} ${props.className}`;
 	const { data: session, status } = useSession();
 	const eventUrl = lang === "pl" ? "/wydarzenia" : "/events";
 	const contactUrl = lang === "pl" ? "/kontakt" : "/contact";
 	const loginUrl = lang === "pl" ? "/logowanie" : "/login";
-	const router = useRouter();
-
-	console.log(contactUrl);
-
-	const handleEventClick = (path, e) => {
-		e.preventDefault();
-		router.push(`/${path}`, undefined, { shallow: true });
-	};
 
 	const logoutHandler = e => {
 		e.preventDefault();
@@ -33,36 +22,24 @@ const NavbarDesktop = ({ disc, lang, ...props }) => {
 		<nav className={classes}>
 			<ul className={styles["nav__list"]}>
 				<li>
-					<Link
-						href='/'
-						// onClick={handleEventClick}
-						className={styles["nav__list-link"]}>
+					<Link href='/' className={styles["nav__list-link"]}>
 						{trl_home}
 					</Link>
 				</li>
 				<li>
-					<a
-						href='/events'
-						onClick={e => handleEventClick("wydarzenia", e)}
-						className={styles["nav__list-link"]}>
+					<Link href={eventUrl} className={styles["nav__list-link"]}>
 						{trl_events}
-					</a>
+					</Link>
 				</li>
 
 				<li>
-					<a
-						href={contactUrl}
-						onClick={e => handleEventClick("kontakt", e)}
-						className={styles["nav__list-link"]}>
+					<Link href={contactUrl} className={styles["nav__list-link"]}>
 						{trl_contact}
-					</a>
+					</Link>
 				</li>
 				<li>
 					{!session && (
-						<Link
-							href={loginUrl}
-							onClick={e => handleEventClick("logowanie", e)}
-							className={styles["nav__list-link"]}>
+						<Link href={loginUrl} className={styles["nav__list-link"]}>
 							{trl_login}
 						</Link>
 					)}
