6f958e50c528e19c14bce500b31d73553636ac55
poncy <poncyman@gmail.com>
2023-10-16 20:37:54 +0200

    feat: component: Carousel - Added reverse on click

--- src/components/transitions/Carousel/Carousel.jsx
+++ src/components/transitions/Carousel/Carousel.jsx
@@ -8,6 +8,23 @@ import { useState, useEffect } from "react";
 
 const Carousel = ({ btn_checkEvents, btn_createEvents, lang }) => {
 	const isClient = typeof window !== "undefined";
+	const classItem = styles["carousel__item"];
+
+	const classesNameConst = [
+		`${classItem} ${styles["carousel__item--left"]}`,
+		`${classItem} ${styles["carousel__item--middle"]}`,
+		`${classItem} ${styles["carousel__item--right"]}`,
+		`${classItem} ${styles["carousel__item--left-reverse"]}`,
+		`${classItem} ${styles["carousel__item--right-reverse"]}`,
+	];
+
+	const [counter, setCounter] = useState(0);
+	const [carouselItems, setCarouselItems] = useState([
+		classesNameConst[0],
+		classesNameConst[1],
+		classesNameConst[2],
+	]);
+
 	const [timer1, setTimer1] = useState(10000);
 	const [block, setBlock] = useState(false);
 	const path = usePathname();
@@ -19,25 +36,54 @@ const Carousel = ({ btn_checkEvents, btn_createEvents, lang }) => {
 	const url_pathDependent =
 		endOfPath === "events" ? "/events/new-event#form" : "/events";
 
-	const changeMiddleHandler = () => {
+	const changeMiddleHandler = direction => {
 		if (isClient && !block) {
+			setCounter(prev => (direction === "right" ? prev + 1 : 1));
 			setBlock(true);
 
-			const itemOne = document.querySelector(
-				`.${styles["carousel__item--left"]}`
-			);
-			const itemTwo = document.querySelector(
-				`.${styles["carousel__item--middle"]}`
-			);
-			const itemThree = document.querySelector(
-				`.${styles["carousel__item--right"]}`
-			);
-			itemOne.classList.remove(styles["carousel__item--left"]);
-			itemTwo.classList.remove(styles["carousel__item--middle"]);
-			itemThree.classList.remove(styles["carousel__item--right"]);
-			itemOne.classList.add(styles["carousel__item--middle"]);
-			itemTwo.classList.add(styles["carousel__item--right"]);
-			itemThree.classList.add(styles["carousel__item--left"]);
+			console.log(counter);
+
+			setCarouselItems(prevItems => {
+				const updatedItems = [...prevItems];
+				let itemOneIndex = updatedItems.findIndex(
+					item => item === classesNameConst[0] || item === classesNameConst[3]
+				);
+				let itemTwoIndex = updatedItems.findIndex(
+					item => item === classesNameConst[1]
+				);
+				let itemThreeIndex = updatedItems.findIndex(
+					item => item === classesNameConst[2] || item === classesNameConst[4]
+				);
+
+				if (
+					(counter === 0 && direction === "right") ||
+					(counter > 0 && direction === "left")
+				) {
+					updatedItems[itemOneIndex] =
+						direction === "right" ? classesNameConst[3] : classesNameConst[0];
+					updatedItems[itemTwoIndex] =
+						direction === "right" ? classesNameConst[4] : classesNameConst[2];
+					updatedItems[itemThreeIndex] = classesNameConst[1];
+
+					if (direction === "left") {
+						setCounter(0);
+					}
+
+					return updatedItems;
+				}
+
+				if (direction === "left") {
+					setCounter(0);
+				}
+
+				updatedItems[itemOneIndex] = classesNameConst[1];
+				updatedItems[itemTwoIndex] =
+					direction === "right" ? classesNameConst[4] : classesNameConst[2];
+				updatedItems[itemThreeIndex] =
+					direction === "right" ? classesNameConst[3] : classesNameConst[0];
+
+				return updatedItems;
+			});
 
 			setTimeout(() => setBlock(false), 1200);
 		}
@@ -56,8 +102,8 @@ const Carousel = ({ btn_checkEvents, btn_createEvents, lang }) => {
 				id='one'
 				src='/images/events/audience.jpg'
 				alt='dsad'
-				className={`${styles["carousel__item"]} ${styles["carousel__item--left"]}`}
-				onClick={changeMiddleHandler}>
+				className={carouselItems[0]}
+				onClick={() => changeMiddleHandler("left")}>
 				<div className={styles["carousel__item-time"]}>
 					<time dateTime='2018-07-07'>
 						<span>1</span>
@@ -72,8 +118,7 @@ const Carousel = ({ btn_checkEvents, btn_createEvents, lang }) => {
 				id='two'
 				alt='dasda'
 				src='/images/events/confetti.jpg'
-				className={`${styles["carousel__item"]} ${styles["carousel__item--middle"]}`}
-				onClick={changeMiddleHandler}>
+				className={carouselItems[1]}>
 				<div className={styles["carousel__item-time"]}>
 					<time>
 						<span>18</span>
@@ -88,8 +133,8 @@ const Carousel = ({ btn_checkEvents, btn_createEvents, lang }) => {
 				id='three'
 				alt='dasd'
 				src='/images/events/woman.jpg'
-				className={`${styles["carousel__item"]} ${styles["carousel__item--right"]}`}
-				onClick={changeMiddleHandler}>
+				className={carouselItems[2]}
+				onClick={() => changeMiddleHandler("right")}>
 				<div className={styles["carousel__item-time"]}>
 					<time>
 						<span>27</span>
@@ -102,10 +147,12 @@ const Carousel = ({ btn_checkEvents, btn_createEvents, lang }) => {
 			</CarouselItem>
 			<LinkAsBtn
 				href={`${url_pathDependent}`}
-				variant='green'
 				className={styles["carousel__event-link"]}>
 				{btn_pathDependent}
 			</LinkAsBtn>
+
+			<p onClick={() => changeMiddleHandler("left")}>lewo</p>
+			<p onClick={() => changeMiddleHandler("right")}>prawo</p>
 		</div>
 	);
 };
