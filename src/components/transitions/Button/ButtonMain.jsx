cc3d076552399e2f2b2241c6cd59830d0326910f
poncy <poncyman@gmail.com>
2023-10-17 22:09:52 +0200

    feat: component: LinkAsButton - added animation loading

--- src/components/transitions/Button/ButtonMain.jsx
+++ src/components/transitions/Button/ButtonMain.jsx
@@ -1,20 +1,36 @@
+"use client";
+import { usePathname } from "next/navigation";
 import styles from "../../../styles/components/transitions/Buttons/ButtonMain.module.scss";
-import btnVariant from "../../../styles/components/transitions/Buttons/ButtonVariants.module.scss";
+import { useState, useEffect } from "react";
+
 /** 
 @param variant - default, borderIn, btnSkewRight
 **/
 
-const ButtonMain = ({ className, variant, ...props }) => {
-	const classes = `${styles.button} ${btnVariant[`${variant}`]} ${
-		className || ""
-	}`;
+const ButtonMain = ({ className, variant, children, ...props }) => {
+	const [isAnimation, setIsAnimation] = useState(false);
+
+	const classesBtn = isAnimation
+		? `${styles.button} ${styles["button__transparent"]}`
+		: styles.button;
+
+	const classesSpan = isAnimation
+		? `${styles["button__border"]} ${styles["rotating-border"]}`
+		: styles["button__border"];
+
+	const animationHandler = () => {
+		props.onClick;
+		setIsAnimation(true);
+	};
+
 	return (
 		<button
-			className={classes}
-			onClick={props.onClick}
+			className={classesBtn}
+			onClick={animationHandler}
 			type={props.type}
 			placeholder={props.placeholder}>
-			{props.children}
+			{children}
+			<span className={classesSpan}>{children}</span>
 		</button>
 	);
 };
--- src/components/transitions/Link/LinkAsBtn.jsx
+++ src/components/transitions/Link/LinkAsBtn.jsx
@@ -1,14 +1,38 @@
+"use client";
 import Link from "next/link";
 import styles from "../../../styles/components/transitions/Link/LinkAsButton.module.scss";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const LinkAsBtn = ({ href, children, className, variant, ...props }) => {
+	const [isAnimation, setIsAnimation] = useState(false);
+
+	const pathname = usePathname();
+
+	useEffect(() => {
+		setIsAnimation(false);
+	}, [pathname]);
+
+	const classesLink = isAnimation
+		? `${styles.link} ${styles["link__transparent"]}`
+		: styles.link;
+
 	const classes = variant
 		? `${styles.link} ${className} ${styles[variant]}`
-		: `${styles.link} ${className} `;
+		: `${classesLink}  ${className} `;
+
+	const classesSpan = isAnimation
+		? `${styles["link__border"]} ${styles["rotating-border"]}`
+		: styles["link__border"];
+
+	const animationHandler = e => {
+		setIsAnimation(true);
+	};
 
 	return (
-		<Link href={href} className={classes}>
+		<Link href={href} className={classes} onClick={animationHandler}>
 			{children}
+			<span className={classesSpan}>{children}</span>
 		</Link>
 	);
 };
