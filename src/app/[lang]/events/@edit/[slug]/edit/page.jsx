b886014a2a1e604b4bd861af0aa797ab5c13d551
poncy <poncyman@gmail.com>
2023-10-23 02:17:10 +0200

    test x5

--- src/app/[lang]/events/@edit/[slug]/edit/page.jsx
+++ src/app/[lang]/events/@edit/[slug]/edit/page.jsx
@@ -1,28 +1,30 @@
 import ModalParallel from "@/components/transitions/Modal/ModalParallel";
 // import FormikEvent from "@/components/transitions/Forms/FormikEvent/FormikEvent";
-// import { getDictionaryElements } from "@/app/dictionaries/rest/dictionaries";
-// import { getDictionaryNotifi } from "@/app/dictionaries/notifications/dictionaries";
+import { getDictionaryElements } from "@/app/dictionaries/rest/dictionaries";
+import { getDictionaryNotifi } from "@/app/dictionaries/notifications/dictionaries";
 
 export default async function Test({
 	params: { lang },
 	searchParams,
 	...props
 }) {
-	// const dict = await getDictionaryElements(lang);
-	// const dictNotifi = await getDictionaryNotifi(lang);
+	const dict = await getDictionaryElements(lang);
+	const dictNotifi = await getDictionaryNotifi(lang);
 
-	// const formTranslation = {
-	// 	trl_title: dict.events.editEvent.form.title,
-	// 	trl_eventTitle: dict.events.newEvent.form.eventTitle,
-	// 	trl_town: dict.events.newEvent.form.town,
-	// 	trl_codePost: dict.events.newEvent.form.codePost,
-	// 	trl_street: dict.events.newEvent.form.street,
-	// 	trl_date: dict.events.newEvent.form.date,
-	// 	trl_startTime: dict.events.newEvent.form.startTime,
-	// 	trl_picture: dict.events.newEvent.form.picture,
-	// 	trl_eventDesc: dict.events.newEvent.form.eventDesc,
-	// 	trl_btn_createEvent: dict.events.editEvent.form.btn_confirm,
-	// };
+	const formTranslation = {
+		trl_title: dict.events.editEvent.form.title,
+		trl_eventTitle: dict.events.newEvent.form.eventTitle,
+		trl_town: dict.events.newEvent.form.town,
+		trl_codePost: dict.events.newEvent.form.codePost,
+		trl_street: dict.events.newEvent.form.street,
+		trl_date: dict.events.newEvent.form.date,
+		trl_startTime: dict.events.newEvent.form.startTime,
+		trl_picture: dict.events.newEvent.form.picture,
+		trl_eventDesc: dict.events.newEvent.form.eventDesc,
+		trl_btn_createEvent: dict.events.editEvent.form.btn_confirm,
+	};
+
+	console.log(formTranslation);
 
 	// const trl_error = dictNotifi.notifications.newEvent.generalError;
 
