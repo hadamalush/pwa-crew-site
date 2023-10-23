b36ec5245dd2c57d410677a39cb20d44984f650a
poncy <poncyman@gmail.com>
2023-10-23 02:10:59 +0200

    test x3

--- src/app/[lang]/events/@edit/[slug]/edit/page.jsx
+++ src/app/[lang]/events/@edit/[slug]/edit/page.jsx
@@ -1,42 +1,43 @@
-import ModalParallel from "@/components/transitions/Modal/ModalParallel";
-import FormikEvent from "@/components/transitions/Forms/FormikEvent/FormikEvent";
-import { getDictionaryElements } from "@/app/dictionaries/rest/dictionaries";
-import { getDictionaryNotifi } from "@/app/dictionaries/notifications/dictionaries";
+// import ModalParallel from "@/components/transitions/Modal/ModalParallel";
+// import FormikEvent from "@/components/transitions/Forms/FormikEvent/FormikEvent";
+// import { getDictionaryElements } from "@/app/dictionaries/rest/dictionaries";
+// import { getDictionaryNotifi } from "@/app/dictionaries/notifications/dictionaries";
 
 export default async function Test({
 	params: { lang },
 	searchParams,
 	...props
 }) {
-	const dict = await getDictionaryElements(lang);
-	const dictNotifi = await getDictionaryNotifi(lang);
+	// const dict = await getDictionaryElements(lang);
+	// const dictNotifi = await getDictionaryNotifi(lang);
 
-	const formTranslation = {
-		trl_title: dict.events.editEvent.form.title,
-		trl_eventTitle: dict.events.newEvent.form.eventTitle,
-		trl_town: dict.events.newEvent.form.town,
-		trl_codePost: dict.events.newEvent.form.codePost,
-		trl_street: dict.events.newEvent.form.street,
-		trl_date: dict.events.newEvent.form.date,
-		trl_startTime: dict.events.newEvent.form.startTime,
-		trl_picture: dict.events.newEvent.form.picture,
-		trl_eventDesc: dict.events.newEvent.form.eventDesc,
-		trl_btn_createEvent: dict.events.editEvent.form.btn_confirm,
-	};
+	// const formTranslation = {
+	// 	trl_title: dict.events.editEvent.form.title,
+	// 	trl_eventTitle: dict.events.newEvent.form.eventTitle,
+	// 	trl_town: dict.events.newEvent.form.town,
+	// 	trl_codePost: dict.events.newEvent.form.codePost,
+	// 	trl_street: dict.events.newEvent.form.street,
+	// 	trl_date: dict.events.newEvent.form.date,
+	// 	trl_startTime: dict.events.newEvent.form.startTime,
+	// 	trl_picture: dict.events.newEvent.form.picture,
+	// 	trl_eventDesc: dict.events.newEvent.form.eventDesc,
+	// 	trl_btn_createEvent: dict.events.editEvent.form.btn_confirm,
+	// };
 
-	const trl_error = dictNotifi.notifications.newEvent.generalError;
+	// const trl_error = dictNotifi.notifications.newEvent.generalError;
 
 	return (
-		<ModalParallel searchParams={searchParams}>
-			{/* <FormikEvent
-				dict={formTranslation}
-				trl_error={trl_error}
-				lang={lang}
-				searchParams={searchParams}
-				variant='edit'
-				scroll='block'
-			/> */}
-			dasdasd
-		</ModalParallel>
+		<div>dsaddddddddddddddd</div>
+		// <ModalParallel searchParams={searchParams}>
+		// 	{/* <FormikEvent
+		// 		dict={formTranslation}
+		// 		trl_error={trl_error}
+		// 		lang={lang}
+		// 		searchParams={searchParams}
+		// 		variant='edit'
+		// 		scroll='block'
+		// 	/> */}
+		// 	dasdasd
+		// </ModalParallel>
 	);
 }
