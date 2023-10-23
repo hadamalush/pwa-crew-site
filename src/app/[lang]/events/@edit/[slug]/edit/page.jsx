b9d3cdbefc3be79fbcb040de39444e5c82389177
poncy <poncyman@gmail.com>
2023-10-23 02:46:32 +0200

    test x7

--- src/app/[lang]/events/@edit/[slug]/edit/page.jsx
+++ src/app/[lang]/events/@edit/[slug]/edit/page.jsx
@@ -10,40 +10,41 @@ export default async function Test({
 }) {
 	let dict, dictNotifi;
 
-	try {
-		dict = await getDictionaryElements(lang);
-		dictNotifi = await getDictionaryNotifi(lang);
-	} catch (err) {
-		console.log(err);
-	}
+	// try {
+	// 	dict = await getDictionaryElements(lang);
+	// 	dictNotifi = await getDictionaryNotifi(lang);
+	// } catch (err) {
+	// 	console.log(err);
+	// }
 
-	const formTranslation = {
-		trl_title: dict.events.editEvent.form.title,
-		trl_eventTitle: dict.events.newEvent.form.eventTitle,
-		trl_town: dict.events.newEvent.form.town,
-		trl_codePost: dict.events.newEvent.form.codePost,
-		trl_street: dict.events.newEvent.form.street,
-		trl_date: dict.events.newEvent.form.date,
-		trl_startTime: dict.events.newEvent.form.startTime,
-		trl_picture: dict.events.newEvent.form.picture,
-		trl_eventDesc: dict.events.newEvent.form.eventDesc,
-		trl_btn_createEvent: dict.events.editEvent.form.btn_confirm,
-	};
+	// const formTranslation = {
+	// 	trl_title: dict.events.editEvent.form.title,
+	// 	trl_eventTitle: dict.events.newEvent.form.eventTitle,
+	// 	trl_town: dict.events.newEvent.form.town,
+	// 	trl_codePost: dict.events.newEvent.form.codePost,
+	// 	trl_street: dict.events.newEvent.form.street,
+	// 	trl_date: dict.events.newEvent.form.date,
+	// 	trl_startTime: dict.events.newEvent.form.startTime,
+	// 	trl_picture: dict.events.newEvent.form.picture,
+	// 	trl_eventDesc: dict.events.newEvent.form.eventDesc,
+	// 	trl_btn_createEvent: dict.events.editEvent.form.btn_confirm,
+	// };
 
-	console.log(formTranslation);
+	// console.log(formTranslation);
 
-	const trl_error = dictNotifi.notifications.newEvent.generalError;
+	// const trl_error = dictNotifi.notifications.newEvent.generalError;
 
 	return (
 		<ModalParallel searchParams={searchParams}>
-			<FormikEvent
+			{/* <FormikEvent
 				dict={formTranslation}
 				trl_error={trl_error}
 				lang={lang}
 				searchParams={searchParams}
 				variant='edit'
 				scroll='block'
-			/>
+			/> */}
+			{lang}
 		</ModalParallel>
 	);
 }
