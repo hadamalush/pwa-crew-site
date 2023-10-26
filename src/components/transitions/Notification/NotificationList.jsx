a5638be070b9d2bc4f1945b41526362cf7146ad4
poncy <poncyman@gmail.com>
2023-10-26 23:58:50 +0200

    feat: added pagination to notificationList

--- src/components/transitions/Notification/NotificationList.jsx
+++ src/components/transitions/Notification/NotificationList.jsx
@@ -6,14 +6,29 @@ import { useState } from "react";
 
 const NotificationList = ({ notifications }) => {
 	const [currentPage, setCurrentPage] = useState(1);
-	const [notifiPerPage, setNotifiPerPage] = useState(5);
+	const [notifiPerPage, setNotifiPerPage] = useState(2);
 
 	const keys = Object.keys(notifications);
-	const data = Object.values(notifications);
+	const allNotices = Object.values(notifications);
+	const maxPage = Math.ceil(allNotices.length / notifiPerPage);
+
+	console.log(maxPage);
 
 	const lastNotifiIndex = currentPage * notifiPerPage;
 	const firstNotifiIndex = lastNotifiIndex - notifiPerPage;
-	const currentsNotifications = data.slice(firstNotifiIndex, lastNotifiIndex);
+	const currentsNotifications = allNotices.slice(
+		firstNotifiIndex,
+		lastNotifiIndex
+	);
+
+	const classPagItem = styles["notifications__pag-item"];
+	const classesPagItemActive = `${classPagItem} ${styles["notifications__pag-item--active"]}`;
+
+	const middlePagNumberLower =
+		([1, 2, 3].includes(currentPage) && maxPage >= 3) ||
+		(currentPage === 4 && maxPage === currentPage);
+	const middlePagNumberHigher =
+		currentPage > 3 && maxPage >= 6 && currentPage + 3 <= maxPage;
 
 	let i = 0;
 
@@ -35,6 +50,65 @@ const NotificationList = ({ notifications }) => {
 					/>
 				);
 			})}
+
+			<ul className={styles.notifications__pag}>
+				<li className={currentPage === 1 ? classesPagItemActive : classPagItem}>
+					1
+				</li>
+				{maxPage >= 2 && (
+					<li
+						className={currentPage === 2 ? classesPagItemActive : classPagItem}>
+						{((maxPage >= 2 && currentPage <= 3) || [4, 5].includes(maxPage)) &&
+							"2"}
+						{maxPage >= 6 && currentPage >= 4 && "..."}
+					</li>
+				)}
+				{maxPage >= 3 && (
+					<li
+						className={
+							currentPage === 3 ||
+							(currentPage + 2 <= maxPage && ![1, 2].includes(currentPage))
+								? classesPagItemActive
+								: classPagItem
+						}>
+						{middlePagNumberLower && "3"}
+						{middlePagNumberHigher && !middlePagNumberLower && currentPage}
+						{!middlePagNumberHigher &&
+							!middlePagNumberLower &&
+							maxPage > 4 &&
+							maxPage > 2 &&
+							maxPage - 2}
+					</li>
+				)}
+
+				{maxPage >= 4 && (
+					<li
+						className={
+							(currentPage === maxPage - 1 && currentPage > 3) ||
+							(maxPage === 4 && currentPage === maxPage)
+								? classesPagItemActive
+								: classPagItem
+						}>
+						{[4, 5].includes(maxPage) && maxPage <= 5 && "4"}
+						{currentPage + 2 < maxPage && maxPage > 6 && "..."}
+						{[currentPage + 2, currentPage + 1, currentPage].includes(
+							maxPage
+						) &&
+							maxPage >= 5 &&
+							maxPage - 1}
+					</li>
+				)}
+				{maxPage >= 5 && (
+					<li
+						className={
+							currentPage === maxPage && maxPage >= 5
+								? classesPagItemActive
+								: classPagItem
+						}>
+						{maxPage >= 5 && maxPage}
+					</li>
+				)}
+			</ul>
 		</ul>
 	);
 };
