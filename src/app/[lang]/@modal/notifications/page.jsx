03a7d7fd18f5bfd2d46058c15df1529bc625f0b7
poncy <poncyman@gmail.com>
2023-10-27 04:43:31 +0200

    test 2x

--- src/app/[lang]/@modal/notifications/page.jsx
+++ src/app/[lang]/@modal/notifications/page.jsx
@@ -13,7 +13,7 @@ export default async function NotificationModal({ searchParams, ...props }) {
 	const session = await getServerSession();
 	const email = session?.user?.email;
 
-	const notifications = await getData(email);
+	// const notifications = await getData(email);
 
 	// try {
 	// 	dict = await getDictionaryElements(lang);
@@ -29,24 +29,24 @@ export default async function NotificationModal({ searchParams, ...props }) {
 	);
 }
 
-const getData = cache(async email => {
-	const timestamp = Date.now();
-	const apiUrl = `https://pwa-crew-site-demo.vercel.app/api/getNotifications?timestamp=${timestamp}&email=${
-		email || null
-	}`;
-	let notifications;
+// const getData = cache(async email => {
+// 	const timestamp = Date.now();
+// 	const apiUrl = `https://pwa-crew-site-demo.vercel.app/api/getNotifications?timestamp=${timestamp}&email=${
+// 		email || null
+// 	}`;
+// 	let notifications;
 
-	try {
-		const response = await fetch(apiUrl, {
-			next: { revalidate: 0 },
-		});
+// 	try {
+// 		const response = await fetch(apiUrl, {
+// 			next: { revalidate: 0 },
+// 		});
 
-		const data = await response.json();
+// 		const data = await response.json();
 
-		notifications = data?.notifications;
-	} catch (err) {
-		console.log(err);
-	}
+// 		notifications = data?.notifications;
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
 
-	return notifications;
-});
+// 	return notifications;
+// });
