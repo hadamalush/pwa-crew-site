165d44bdd6e7e2948531ba4bdc8ac210f3f9b2d8
poncy <poncyman@gmail.com>
2023-11-01 13:06:42 +0100

    writing effect has been set only on the home page

--- src/components/Content/HomeStartContent.jsx
+++ src/components/Content/HomeStartContent.jsx
@@ -3,7 +3,7 @@ import WrapperStart from "../transitions/Wrappers/WrapperStart";
 import LinkAsBtn from "../transitions/Link/LinkAsBtn";
 import styles from "../../styles/components/Content/HomeStartContent.module.scss";
 import { useSession } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
@@ -19,15 +19,19 @@ const HomeStartContent = ({
 }) => {
 	const { data: session, status } = useSession();
 	const params = useSearchParams();
+	const pathname = usePathname();
+	const isHomePage = pathname === "/en" || pathname === "/pl";
+
 	const isLoginLink = !session ? "/registration" : "/events";
 	const isLoginLinkNameBtn = !session ? btn_registration : btn_events;
 	const classes = `${styles.introduction} ${className || ""}`;
 	const refresh = params.get("refresh");
 	const router = useRouter();
 	const classesTitle = `${styles["introduction__h1"]} ${
-		lang === "pl"
+		isHomePage &&
+		(lang === "pl"
 			? styles["introduction__h1--pl"]
-			: styles["introduction__h1--en"]
+			: styles["introduction__h1--en"])
 	}`;
 
 	useEffect(() => {
@@ -39,7 +43,7 @@ const HomeStartContent = ({
 	return (
 		<WrapperStart className={classes}>
 			<div className={styles["introduction__text"]}>
-				<div className={styles["introduction__loader"]}>
+				<div className={isHomePage ? styles["introduction__loader"] : ""}>
 					<h1 className={classesTitle}>{title}</h1>
 				</div>
 				<p className={styles["introduction__text"]}>{text}</p>
