7d65eec4fbaf147af221cb792260f39007168b63
poncy <poncyman@gmail.com>
2023-12-05 18:52:12 +0100

    test: messaging api testing - cyclic sending added

--- src/app/api/admin/inbox/route.js
+++ src/app/api/admin/inbox/route.js
@@ -2,7 +2,13 @@ import cors from "@/lib/admin/core";
 import { NextResponse } from "next/server";
 import { google } from "googleapis";
 
-export async function GET(req) {
+export async function POST(req) {
+  const data = await req.json();
+  console.log(data);
+
+  const label = data.label;
+  const pageToken = data.pageToken;
+
   const oAuth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_ID_CLIENT_GMAIL,
     process.env.GOOGLE_SECRET_CLIENT_GMAIL,
@@ -10,6 +16,7 @@ export async function GET(req) {
   );
   let allMessages = [];
   let messageData, res;
+  let newPageToken;
 
   try {
     oAuth2Client.setCredentials({
@@ -18,87 +25,75 @@ export async function GET(req) {
 
     const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
 
-    const labels = ["SPAM", "TRASH", "INBOX"];
+    res = await gmail.users.messages.list({
+      userId: "me",
+      labelIds: label,
+      maxResults: 15,
+      pageToken: pageToken || null,
+    });
 
-    for (let i = 0; i < 3; i++) {
-      // console.log(label);
-      let hasNextPage = true;
-      let pageTokens = {};
+    for (let message of res.data.messages) {
+      let newMsg = {};
 
-      while (hasNextPage) {
-        res = await gmail.users.messages.list({
-          userId: "me",
-          labelIds: [labels[i]],
-          maxResults: 15,
-          pageToken: pageTokens[labels[i]] || null,
-        });
-        // console.log(res.data.messages);
+      messageData = await gmail.users.messages.get({ userId: "me", id: message.id });
+      newMsg.id = message.id;
+      newMsg.description = messageData.data.snippet;
+      newMsg.date = new Date(Number(messageData.data.internalDate)).toString();
 
-        for (let message of res.data.messages) {
-          let newMsg = {};
+      // Set email
+      const email = messageData.data.payload.headers
+        .find((header) => header.name === "From")
+        .value.match(/<(.*)>/)[1];
 
-          messageData = await gmail.users.messages.get({ userId: "me", id: message.id });
-          newMsg.id = message.id;
-          newMsg.description = messageData.data.snippet;
-          newMsg.date = new Date(Number(messageData.data.internalDate)).toString();
+      newMsg.owner = email;
+      newMsg.email = email;
 
-          // Set email
-          const email = messageData.data.payload.headers
-            .find((header) => header.name === "From")
-            .value.match(/<(.*)>/)[1];
+      // Set subject
+      newMsg.subject = messageData.data.payload.headers.find(
+        (header) => header.name === "Subject"
+      ).value;
 
-          newMsg.owner = email;
-          newMsg.email = email;
-
-          // Set subject
-          newMsg.subject = messageData.data.payload.headers.find(
-            (header) => header.name === "Subject"
-          ).value;
-
-          if (messageData.data.labelIds.includes("STARRED")) {
-            newMsg.isFeatured = true;
-          } else {
-            newMsg.isFeatured = false;
-          }
-          if (messageData.data.labelIds.includes("UNREAD")) {
-            newMsg.unRead = true;
-          } else {
-            newMsg.unRead = false;
-          }
-          if (messageData.data.labelIds.includes("SPAM")) {
-            newMsg.isInSpam = true;
-          } else {
-            newMsg.isInSpam = false;
-          }
-          if (messageData.data.labelIds.includes("TRASH")) {
-            newMsg.isInTrash = true;
-          } else {
-            newMsg.isInTrash = false;
-          }
-
-          allMessages.push(newMsg);
-        }
-
-        if (res.data?.nextPageToken) {
-          pageTokens[labels[i]] = res?.data?.nextPageToken;
-        } else {
-          hasNextPage = false;
-        }
+      if (messageData.data.labelIds.includes("STARRED")) {
+        newMsg.isFeatured = true;
+      } else {
+        newMsg.isFeatured = false;
       }
+      if (messageData.data.labelIds.includes("UNREAD")) {
+        newMsg.unRead = true;
+      } else {
+        newMsg.unRead = false;
+      }
+      if (messageData.data.labelIds.includes("SPAM")) {
+        newMsg.isInSpam = true;
+      } else {
+        newMsg.isInSpam = false;
+      }
+      if (messageData.data.labelIds.includes("TRASH")) {
+        newMsg.isInTrash = true;
+      } else {
+        newMsg.isInTrash = false;
+      }
+
+      allMessages.push(newMsg);
+    }
+
+    if (res.data?.nextPageToken) {
+      // pageTokens[labels[i]] = res?.data?.nextPageToken;
+      newPageToken = res.data.nextPageToken;
     }
   } catch (err) {
     NextResponse.json({ message: "Something went wrong" }, { status: 502 });
   }
 
-  // console.log(res);
-  // console.log(messageData);
-
   return cors(
     req,
-    NextResponse.json(allMessages, {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    })
+    NextResponse.json(
+      { allMessages, label, newPageToken },
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    )
   );
 }
 
