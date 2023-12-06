b5fb2e33fa4730ede7c1a801e3ce44e94fe8bf3a
poncy <poncyman@gmail.com>
2023-12-06 01:33:44 +0100

    feat: added handling for delete messages for rest api (api/admin/inbox/deletePermanent)

---
+++ src/app/api/admin/inbox/deletePermanent/route.js
@@ -0,0 +1,59 @@
+import cors from "@/lib/admin/core";
+import { NextResponse } from "next/server";
+import { google } from "googleapis";
+
+export async function POST(req) {
+  const messages = await req.json();
+
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_ID_CLIENT_GMAIL,
+    process.env.GOOGLE_SECRET_CLIENT_GMAIL,
+    process.env.GOOGLE_CALLBACK_URI_GMAIL
+  );
+
+  oAuth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_TOKEN_GMAIL,
+  });
+
+  const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
+
+  try {
+    for (let messageId of messages) {
+      await gmail.users.messages.delete({
+        userId: "me",
+        id: messageId,
+      });
+    }
+  } catch (err) {
+    return cors(
+      req,
+      NextResponse.json(
+        { error: "Failed to delete messages" },
+        {
+          status: 504,
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+    );
+  }
+
+  return cors(
+    req,
+    NextResponse.json(
+      { message: `Messages have been deleted` },
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+  );
+}
+
+export async function OPTIONS(request) {
+  return cors(
+    request,
+    new Response(null, {
+      status: 204,
+    })
+  );
+}
